1105292342211d6677990ee5c3af0c7ad5c22ef7
Carolina Abreu <aabreu.pitang@ons.org.br>
2024-09-13 08:38:40 -0300

    - Ajuste de arquivos e mapeamentos

--- TechStackBack/Models/TechStack.cs
+++ TechStackBack/Models/TechStack.cs
@@ -8,7 +8,7 @@ namespace TechStackProcesso.Models
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
-        public string Area { get; set; }
+        public string Tipo { get; set; }
         public DateTime DataCriacao { get; set; }
 
         public ICollection<AreaConhecimento> AreasConhecimento { get; set; }
